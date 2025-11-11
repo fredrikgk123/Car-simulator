e4ad8e085d2ef9816819239f58947b5008a63d3d
Fredrikgk123 <fredgk@stud.ntnu.no>
2025-11-11 11:15:11 +0100

    added manager for game objects

---
+++ src/core/game_object_manager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "../core/game_object.hpp"
+
+class GameObjectManager {
+public:
+    virtual ~GameObjectManager() = default;
+
+    // Update all managed objects
+    virtual void update(float deltaTime) = 0;
+
+    // Handle collisions with the vehicle
+    virtual void handleCollisions(class Vehicle& vehicle) = 0;
+
+    // Reset all objects to initial state
+    virtual void reset() = 0;
+
+    // Get the number of active objects
+    virtual size_t getCount() const = 0;
+};
